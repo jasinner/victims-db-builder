6a8c19872d8dbad3788f01e88e6e8ad7773ce6e9
Jason Shepherd <jshepher@redhat.com>
2017-11-10 16:32:32 +1000

    finding added files

--- victims_db_builder/repo.py
+++ victims_db_builder/repo.py
@@ -1,6 +1,6 @@
 from sys import argv
 from os import path
-from git import Repo, remote, exc
+from git import Repo, remote, exc, diff
 import ConfigParser
 
 import processor
@@ -11,25 +11,26 @@ repoUrl = config.get('cve_db', 'repo')
 filePath = config.get('cve_db', 'path')
 javaPath = config.get('cve_db', 'javaPath')
 
-def main(argv):
-    if len(argv) != 2:
-        print """Usage: python repo.py <victims-upload-url>"""
+def main():
+    try:
+        repo = Repo(filePath)
+
+    except exc.InvalidGitRepositoryError, e:
+        repo = Repo.clone_from(repoUrl, filePath)
+        #TODO fix submit to work with victims-upload
+        processor.findYamlFiles(repo.working_tree_dir + '/' + javaPath, "", "")
     else:
-        script, uploadUrl = argv
-        #Check if we already have a clone
-        try:
-            repo = Repo(filePath)
-        except exc.InvalidGitRepositoryError, e:
-            repo = Repo.clone_from(repoUrl, filePath)
-            #TODO fix submit to work with victims-upload
-            processor.findYamlFiles(repo.working_tree_dir + '/' + javaPath, "", "")
-        else:
-            doUpdate()
+        doUpdate(repo)
 
 
-def doUpdate():
-    print 'Do update called'
+def doUpdate(repo):
+    diff = repo.index.diff(repo.heads.master.commit)
+    print repo.heads.master.commit
+    for d in diff.iter_change_type('A'):
+        if d.new_file:
+            if javaPath in d.b_path:
+                processor.processReport(d.b_path, "", "")
 
 
 if __name__ == '__main__':
-    main(argv)
+    main()
